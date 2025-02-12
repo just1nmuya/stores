29d7f84eb314ce011aa4f162c59dde82155db3a7
justinmuya <justinnjoroge426@gmail.com>
2025-02-12 16:48:26 +0300

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
@@ -21,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+          <Analytics/>
           <ModalProvider />
           <ToastProvider />
           <Navbar />
