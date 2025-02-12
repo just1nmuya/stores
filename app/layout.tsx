59c7873f248d957571fad4664442f134bf4a281f
justinmuya <justinnjoroge426@gmail.com>
2025-02-12 16:53:41 +0300

    insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
@@ -23,6 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
           <Analytics/>
+          <SpeedInsights/>
           <ModalProvider />
           <ToastProvider />
           <Navbar />
