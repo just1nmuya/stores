3ffc68d057b748eccc923a9266910842339cc5e6
justinmuya <justinnjoroge426@gmail.com>
2025-02-14 17:06:40 +0300

    tab1

--- components/gallery/index.tsx
+++ components/gallery/index.tsx
@@ -13,7 +13,7 @@ interface GalleryProps {
 const Gallery: React.FC<GalleryProps> = ({ images }) => {
   return (
     <TabGroup as="div" className="flex flex-col-reverse">
-      <div className="mx-auto mt-6 hidden w-full max-2xl sm:block lg:max-w-none">
+      <div className="mx-auto mt-6 w-full max-2xl sm:block lg:max-w-none">
         <TabList className="grid grid-cols-4 gap-6">
           {images.map((image) => (
             <GalleryTab key={image.id} image={image} />
